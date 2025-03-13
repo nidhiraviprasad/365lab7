7004820cbc2ac00d9fe71cbde06a0bb510712046
nidhiraviprasad <nidhi.raviprasad@gmail.com>
2025-03-12 18:32:58 -0700

    added some of fr2

--- lab7.py
+++ lab7.py
@@ -1,5 +1,7 @@
 import getpass
 import mysql.connector
+import datetime
+from decimal import Decimal
 
 def connect():
     db_password = getpass.getpass()
@@ -74,6 +76,101 @@ def fr2(conn):
     adults = input("Number of adults: ").strip()
 
     # perform input validation here before continuing with query
+    checkin = checkin.split('/')
+    checkout = checkout.split('/')
+    checkin = datetime.date(int(checkin[2]), int(checkin[0]), int(checkin[1]))
+    checkout = datetime.date(int(checkout[2]), int(checkout[0]), int(checkout[1]))
+
+    if (checkin > checkout):
+        print("Invalid dates: checkin date is after checkout date")
+        return
+    
+    if (not children.isnumeric() or not adults.isnumeric()):
+        print("Please only provide numbers for the number of adults/children categories")
+        return
+    numOccupants = int(children) + int(adults)
+    
+    ci = checkin.strftime("%Y-%m-%d")
+    co = checkout.strftime("%Y-%m-%d")
+
+    cursor = conn.cursor()
+    cursor.execute("""
+select *
+from lab7_rooms r
+where (r.RoomCode = %s or %s = 'Any' or %s = '')
+and (r.bedType = %s or %s = 'Any' or %s = '')
+and r.maxOcc >= %s
+and ( 
+    select count(*)
+    from lab7_reservations res
+    where (r.RoomCode = res.Room or %s = 'Any' or %s = '')
+    and (
+        (res.CheckIn >= %s and res.Checkout <= %s) or
+        (res.CheckIn <= %s and res.Checkout >= %s) or
+        (res.CheckIn <= %s and res.Checkout >= %s)
+        )
+    ) = 0;
+                   """, [code, code, code, bed, bed, bed, str(numOccupants), code, code, ci, co, co, co, ci, ci])
+    
+    room_details = cursor.fetchall()
+    if (room_details):
+        for i in range(len(room_details)):
+            print(f"{i + 1}: {room_details[i]}")
+        room = input("Which of the following rooms would you like to book? Please enter the option number: ")
+        while not room.isnumeric() or int(room) > len(room_details) or int(room) < 1:
+            room = input("Invalid room selection. Please select from the given options: ")
+        room = int(room) - 1
+
+        basePrice = float(room_details[room][5])
+
+        cursor.execute("""
+select max(CODE) from lab7_reservations;
+                       """)
+        result = cursor.fetchall()
+        if result:
+            newCode = str(int(result[0][0]) + 1)
+        else:
+            newCode = 1
+
+        numWeekdays = 0
+        numWeekends = 0
+        currentDate = checkin
+        delta = datetime.timedelta(days=1)
+        while currentDate < checkout:
+            if currentDate.weekday() > 4:
+                numWeekends += 1
+            else:
+                numWeekdays +=1
+            currentDate += delta
+        
+        costOfStay = basePrice * numWeekdays + basePrice * 1.1 * numWeekends
+        costOfStay = "Decimal('" + str(costOfStay) + "')"
+
+        cursor.execute("""
+insert into lab7_reservations 
+(CODE, Room, CheckIn, Checkout, Rate, LastName, FirstName, Adults, Kids)
+values (%s, %s, %s, %s, %s, %s, %s, %s, %s);
+                       """, [newCode, room_details[room][0], ci, co, costOfStay, l_name, f_name, adults, children])
+        
+        
+        print(f"\nConfirmed! {f_name} {l_name} has booked room {room_details[room][0]}: {room_details[room][1]}." +
+              f"\nBed type: {room_details[room][3]}" +
+              f"\nDates: {ci} to {co}" +
+              f"\nOccupants: {adults} adults and {children} children" +
+              f"\nTotal cost: {costOfStay}" + 
+              f"\nConfirmation code: {newCode}\n")
+        
+        
+
+
+            
+    else:
+        print("womp womp")
+
+
+
+
+        
 
 
 # cancel a reservation
@@ -89,15 +186,16 @@ WHERE CODE = %s
     if (result):
         print("Reservation details: ")
         print(result)
-        confirmation = input("Are you sure you would like to cancel the above reservation? [Y to confirm; any other key to cancel]" ).strip()
+        confirmation = input("Are you sure you would like to cancel the above reservation? [Y to confirm; any other key to cancel] " ).strip()
         if (confirmation == 'Y' or confirmation == 'y'):
             cursor.execute("""
 DELETE
 from lab7_reservations
 WHERE CODE = %s
                    """, [code])
+            print(f"Your reservation {code} was cancelled successfully. ")
         else:
-            print("Your reservation was not cancelled.")
+            print("Your reservation was not cancelled. ")
     else:
         print("Your reservation with code", code, "was not found in the system. ")
     cursor.close()
